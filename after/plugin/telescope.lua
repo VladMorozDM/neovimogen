0498682ea0fb480c947c0d1dfd9e1556172698e5
vladmorozdm <vladmorozdm@gmail.com>
2024-12-12 23:49:24 +0200

    added remaps for telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,4 +1,5 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Telescope find git files' })
-
+vim.keymap.set('n', '<leader>b', '<C-^>')
+vim.keymap.set('n', '<C-b>', '<C-^>')
