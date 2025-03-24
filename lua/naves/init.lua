965a61591ade711bce23f05ece34929da72061ad
Naves <nikeblack97@gmail.com>
2025-03-24 13:27:30 +0200

    added macro for upload firmware

--- lua/naves/init.lua
+++ lua/naves/init.lua
@@ -4,3 +4,4 @@ require("naves.set")
 
 local esc = vim.api.nvim_replace_termcodes("<ESC>", true, true, true)
 vim.fn.setreg("p", '^f:lyt]oplatformio run -e '..esc..'p^"+Yddku')
+vim.fn.setreg("P", '^f:lyt]oplatformio run -t upload -e '..esc..'p^"+Yddku')
